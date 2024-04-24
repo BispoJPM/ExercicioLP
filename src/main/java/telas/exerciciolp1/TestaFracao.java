ee951680d80462e0315bdc2b27be811ef8e1d323
BispoJPM <marcondesjpb@gmail.com>
2024-04-23 21:59:10 -0300

    Finalizei!

--- src/main/java/telas/exerciciolp1/TestaFracao.java
+++ src/main/java/telas/exerciciolp1/TestaFracao.java
@@ -1,9 +1,20 @@
 package telas.exerciciolp1;
 
+import java.util.Scanner;
+
 public class TestaFracao {
     public static void main(String[] args) {
-        Fracao fracao1 = new Fracao(8, 2);
-        Fracao fracao2 = new Fracao(9, 4);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o numerador e o denominador da primeira fração separados por espaço:");
+        int numerador1 = scanner.nextInt();
+        int denominador1 = scanner.nextInt();
+        Fracao fracao1 = new Fracao(numerador1, denominador1);
+
+        System.out.println("Digite o numerador e o denominador da segunda fração separados por espaço:");
+        int numerador2 = scanner.nextInt();
+        int denominador2 = scanner.nextInt();
+        Fracao fracao2 = new Fracao(numerador2, denominador2);
 
         System.out.println("Fração 1: " + fracao1);
         System.out.println("Fração 2: " + fracao2);
